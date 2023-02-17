fcab7dcdec8f6764c20c7f2d0713b34f794f2726
Erwin Seribo <erwin_rulezzz87@yahoo.com>
2023-02-17 18:58:07 +0800

    responsive and finalise link

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,4 +9,26 @@ const careerYears = function() {
 
 $(document).ready(function() {
   careerYears();
+
+  $(document).on('scroll', function() {
+    const nowScrollTop = $(this).scrollTop();
+
+    if (nowScrollTop > 700){
+      $('.header-content .navbar').addClass('full');
+    } else {
+      $('.header-content .navbar').removeClass('full');
+    }
+  });
+
+  $('.scroll-to').on('click', function(e) {
+    e.preventDefault();
+    const body = $('body');
+    const target = $($(this).attr('href'));
+    var position = target.offset().top - body.offset().top + body.scrollTop();
+
+    $('#bdNavbar').offcanvas('hide')
+    $('body, html').animate({ scrollTop: position });
+
+    return false;
+  });
 });
